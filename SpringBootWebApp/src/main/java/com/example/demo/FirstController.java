38435f197467c5e078097619f3c880af98cd8506
krishnaMjha <krishna.jha@emc.com>
2018-09-27 21:59:21 +0530

    initial commit

---
+++ SpringBootWebApp/src/main/java/com/example/demo/FirstController.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+//@Controller
+public class FirstController  {
+	
+//	qb[t
+//	 @GetMapping("/greeting")
+//	    public String greeting(@RequestParam(name="name", required=false) String name, @RequestParam(password="psw", required=false) String password, @RequestParam(remember=true, required=false, defaultValue="World") boolean remember boolean remember, Model model) {
+//	        model.addAttribute("name", name);
+//	        return "greeting";
+//	    }
+//	 @RequestMapping(value="/single?user", method = RequestMethod.GET)
+//	    public String nextLoginPage(ModelMap model){
+//	        return "category";
+//	    }
+
+}
